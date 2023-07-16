b65a3661dd78876aa04eebf4d84af85202770515
afshinkhd <afshin90kh@gmail.com>
2023-07-16 09:08:45 +0200

    implemented all alogos except shared - code is dirty!

--- sort_vec.cu
+++ sort_vec.cu
@@ -16,9 +16,10 @@
 #include <stdio.h>
 #include <time.h>
 #include <cuda_runtime.h>
-#define N (256 * 256)
-#define BLKDIM (128)
-#define SHOWITEMS (100)
+#define N (4 * 4)
+#define BLKDIM (2)
+#define BLKSIZE (2)
+#define SHOWITEMS (16)
 
 
 
@@ -52,6 +53,30 @@ void initilizeArray(int* arr) {
 __device__ void _merge(int* arr, int* left, int leftSize, int* right, int rightSize) {
 	int i = 0, j = 0, k = 0;
 	//printf("_merge : lef[0]:%d , arr[0]:%d , right[0]:%d , arr[1]:%d\n", left[0], arr[0], right[0], arr[1]);
+	while (i < leftSize && j < rightSize) {
+		if (left[i] <= right[j]) {
+			//printf("$$$$$$$$$\n");
+			arr[k++] = left[i++];
+			//printf("k:%d\n", k);
+		}
+		else {
+			//printf("%%%%%%%%%%%\n");
+			arr[k++] = right[j++];
+		}
+	}
+
+	while (i < leftSize) {
+		arr[k++] = left[i++];
+	}
+
+	while (j < rightSize) {
+		arr[k++] = right[j++];
+	}
+}
+
+__device__ void _merge_shared(int* arr, int* left, int leftStart, int leftSize, int* right, int rightStart, int rightSize) {
+	int i = leftSize, j = rightStart, k = 0;
+	printf("_merge : lef[0]:%d , left[1]:%d, arr[0]:%d , right[0]:%d ,right[1]:%d, arr[1]:%d\n", left[0],left[1], arr[0], right[0],right[1], arr[1]);
 	while (i < leftSize && j < rightSize) {
 		if (left[i] <= right[j]) {
 			//printf("$$$$$$$$$\n");
@@ -143,6 +168,111 @@ __global__ void mergeSortWithThreads(int* arr, int n) {
 				__syncthreads();
 			
 		}
+	}
+}
+
+__global__ void mergeSortWithThreadsAndBlocks_limited(int* arr, int n) {
+	int currentSize, leftStart;
+	int idx = threadIdx.x + (blockIdx.x * blockDim.x);
+	int partSize = BLKSIZE * blockDim.x;
+	//Warning : partsize * 2 ?????
+	int nPart = (n + partSize - 1) / partSize;
+
+	for (currentSize = 1; currentSize < n; currentSize *= 2) {
+		for (int p = 0; p < nPart; p++) {
+			int leftStart = (idx + (p * partSize)) * 2 * currentSize;
+			//if (currentSize > 512 * 1   ) printf("leftstart: %d , p: %d , n: %d , partSize: %d , nPart: %d ,idx: %d\n", leftStart, p, n , partSize, nPart,idx);
+			if (leftStart + currentSize > n) break;
+			//if (currentSize > 512 * 1 && leftStart > 2095103) printf("SECOND: leftstart: %d , p: %d , n: %d , partSize: %d , nPart: %d ,idx: %d ,currentsize :%d\n", leftStart, p, n, partSize, nPart, idx,currentSize);
+			int mid = leftStart + currentSize - 1;
+			int rightEnd = (leftStart + 2 * currentSize - 1 < n - 1) ? leftStart + 2 * currentSize - 1 : n - 1;
+			int leftSize = mid - leftStart + 1;
+			int rightSize = rightEnd - mid;
+			//if (currentSize > 512 * 1 && leftStart > 2095103) printf("leftsize: %d , rightsize: %d", leftSize, rightSize);
+			//printf("index:%d , mid:%d , rightEnd:%d , leftsize:%d , rightsize:%d\n", index, mid, rightEnd, leftSize, rightSize);
+			int* left = (int*)malloc(leftSize * sizeof(int));
+			int* right = (int*)malloc(rightSize * sizeof(int));
+			//if (currentSize > 512 * 1 && leftStart > 2095103) printf("here1\n");
+			// Copy data from original array to temporary left and right arrays
+			for (int i = 0; i < leftSize; i++) {
+				//if (currentSize > 512 * 1 && leftStart > 2095103) printf("########\n");
+				left[i] = *(arr + leftStart + i);
+			}
+			//if (currentSize > 512 * 1 && leftStart > 2095103) printf("here2\n");
+			for (int i = 0; i < rightSize; i++) {
+				right[i] = *(arr + mid + 1 + i);
+			}
+			//if (currentSize > 512 * 1 && leftStart > 2095103) printf("here3\n");
+			//printf("before : arr0:%d , arr1:%d , arr2:%d , arr3:%d\n", arr[0], arr[1], arr[2], arr[3]);
+			_merge(arr + leftStart, left, leftSize, right, rightSize);
+			//printf("after : arr0:%d , arr1:%d , arr2:%d , arr3:%d\n", arr[0], arr[1], arr[2], arr[3]);
+			free(left);
+			free(right);
+
+			__syncthreads();
+
+		}
+	}
+}
+
+__global__ void mergeSortWithThreadsAndBlocks_limited_shared(int* arr, int n) {
+	int currentSize, leftStart;
+	int idx = threadIdx.x + (blockIdx.x * blockDim.x);
+	int partSize = BLKSIZE * blockDim.x;
+	int nPart = (n + partSize - 1) / (partSize * 2);
+	int blockSize = blockDim.x;
+	__shared__ int* left;
+	__shared__ int* right;
+	//extern __shared__ int sharedArray[];
+
+	for (currentSize = 1; currentSize < n; currentSize *= 2) {
+		const int sharedSize_side =  (blockSize* currentSize * 2 < n) ? blockSize * currentSize : n/2 ;
+		printf("sharedsize_side: %d , currentsize: %d \n", sharedSize_side , currentSize);
+		left = (int*)malloc(sharedSize_side * sizeof(int));
+		right = (int*)malloc(sharedSize_side * sizeof(int));
+		//int* left = &sharedArray[sharedSize_side];
+		//int* right = &sharedArray[sharedSize_side];
+
+		for (int p = 0; p < nPart; p++) {
+			int leftStart = (idx + (p * partSize)) * 2 * currentSize;
+			
+			if (leftStart + currentSize > n) break;
+			printf("leftstart: %d , p: %d , n: %d , partSize: %d , nPart: %d ,idx: %d\n", leftStart, p, n, partSize, nPart, idx);
+			//if (currentSize > 512 * 1 && leftStart > 2095103) printf("SECOND: leftstart: %d , p: %d , n: %d , partSize: %d , nPart: %d ,idx: %d ,currentsize :%d\n", leftStart, p, n, partSize, nPart, idx,currentSize);
+			int mid = leftStart + currentSize - 1;
+			int rightEnd = (leftStart + 2 * currentSize - 1 < n - 1) ? leftStart + 2 * currentSize - 1 : n - 1;
+			int leftSize = mid - leftStart + 1;
+			int rightSize = rightEnd - mid;
+			int rightStart = mid + 1;
+			//if (currentSize > 512 * 1 && leftStart > 2095103) printf("leftsize: %d , rightsize: %d", leftSize, rightSize);
+			//printf("index:%d , mid:%d , rightEnd:%d , leftsize:%d , rightsize:%d\n", index, mid, rightEnd, leftSize, rightSize);
+			//if (currentSize > 512 * 1 && leftStart > 2095103) printf("here1\n");
+			// Copy data from original array to temporary left and right arrays
+			int _unused_idx = 0;
+			for (int i = 0; i < leftSize; i++) {
+				//if (currentSize > 512 * 1 && leftStart > 2095103) printf("########\n");
+				_unused_idx = i % sharedSize_side;
+				left[leftStart % sharedSize_side] = *(arr + leftStart + i);
+			}
+			//if (currentSize > 512 * 1 && leftStart > 2095103) printf("here2\n");
+			for (int i = 0; i < rightSize; i++) {
+				_unused_idx = i % sharedSize_side;
+				right[i % sharedSize_side] = *(arr + rightStart + i);
+			}
+
+			__syncthreads();
+			//if (currentSize > 512 * 1 && leftStart > 2095103) printf("here3\n");
+			printf("before : arr0:%d , arr1:%d , arr2:%d , arr3:%d\n", arr[0], arr[1], arr[2], arr[3]);
+			printf("leftStart: %d , rigfhtStart: %d , sharedSize: %d\n", leftStart, rightStart, sharedSize_side);
+			_merge_shared(arr + leftStart, left , (leftStart% sharedSize_side) , leftSize + leftStart, right,(rightStart% sharedSize_side), rightSize + rightStart);
+			printf("after : arr0:%d , arr1:%d , arr2:%d , arr3:%d\n", arr[0], arr[1], arr[2], arr[3]);
+			__syncthreads();
+	
+		}
+
+		//free(left);
+		//free(right);
+		
 	}
 }
 
@@ -338,7 +468,9 @@ cudaError_t mergeSortWithThreadsHelper(int* arr, size_t size) {
 	tstart = get_time();
 	//mergeSortWithThreads << <1, BLKDIM >> > (d_arr, N);
 	//mergeSortWithBlocks << <N, 1 >> > (d_arr, N);
-	mergeSortWithBlocksAndThreads << <(N + BLKDIM -1 )/ (BLKDIM * 2), BLKDIM >> > (d_arr, N);
+	//mergeSortWithBlocksAndThreads << <(N + BLKDIM -1 )/ (BLKDIM * 2), BLKDIM >> > (d_arr, N);
+	//mergeSortWithThreadsAndBlocks_limited << <BLKSIZE , BLKDIM >> > (d_arr, N);
+	mergeSortWithThreadsAndBlocks_limited_shared << <BLKSIZE , BLKDIM >> > (d_arr, N);
 	//mergeSortWithSharedMemmory << <1, 1, N * sizeof(int) >> > (d_arr, N);
 
 	cudaStatus = cudaGetLastError();
